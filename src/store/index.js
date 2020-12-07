56f12106aac85d9591201ee77857e07e55d70d48
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-12-07 14:41:38 +0100

    ajout du redeux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import reducer from './reducer';
 
 // Store contient notre state, le gère et les protège
 // (ne pas risquer de modifier directement les données)
-const store = createStore(reducer);
+// enhancer est normalement le troisème paramètre de createStore
+const store = createStore(reducer, devToolsEnhancer());
 
 export default store;
